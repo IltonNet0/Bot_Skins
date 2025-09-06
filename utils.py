ab0345367a4d7b84a0c6b700f1487b082147362f
José Ilton <iltonneto2.1@gmail.com>
2025-09-06 00:59:57 -0300

    Refining processes

--- utils.py
+++ utils.py
@@ -14,7 +14,7 @@ locator = {
     'username_input': (By.XPATH, '//*[@id="responsive_page_template_content"]/div[1]/div[1]/div/div/div/div[2]/div/form/div[1]/input'),
     'password_input': (By.XPATH, '//*[@id="responsive_page_template_content"]/div[1]/div[1]/div/div/div/div[2]/div/form/div[2]/input'),
     'login_steam': (By.XPATH, '//*[@id="responsive_page_template_content"]/div[1]/div[1]/div/div/div/div[2]/div/form/div[4]'),
-    'close_sell': (By.XPATH, '//*[@id="headlessui-dialog-panel-:r8m:"]/div[2]/button[1]'),
+    'close_sell': (By.XPATH, '/html/body/div[8]/div/div/div/div[2]/div/div[3]/div[2]/button[1]'),
     'award_keydrop': (By.XPATH,'/html/body/div[1]/main/div[3]/ul/li[1]/button/div/div/canvas'),
 }
 
@@ -22,17 +22,8 @@ def save_cookies(driver, file):
     with open(file, "wb") as f:
         pickle.dump(driver.get_cookies(), f)
 
-
-
-
-def keydrop(driver, row):
-
-    # sleep(5)
-    # save_cookies(driver, "cookies_key-drop.pkl")
-
-    # load_cookies(driver, "cookies_key-drop.pkl")
-
-    with open('cookies_key-drop.pkl', "rb") as f:
+def load_cookies(driver,file):
+    with open(file, "rb") as f:
         cookies = pickle.load(f)
 
     for cookie in cookies:
@@ -40,14 +31,23 @@ def keydrop(driver, row):
 
     driver.refresh()
 
+
+def keydrop(driver, row):
+
+    # sleep(5)
+    # save_cookies(driver, "cookies_key-drop.pkl")
+
+    load_cookies(driver, "cookies_key-drop.pkl")
+
+    redeem_buttom = driver.find_element(*locator['award_keydrop'])
+    driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", redeem_buttom)
+
     try:
         WebDriverWait(driver, 30).until(EC.element_to_be_clickable(locator['close_sell'])).click()
     except:
         WebDriverWait(driver, 30).until(EC.element_to_be_clickable(locator['award_keydrop'])).click()
 
 
-
-    sleep(2)
 
     sleep(5)
 
